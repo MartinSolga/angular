a2035f24cba7b916df0f901f398db3b0abc57b39
Martin <mjsolga@gmail.com>
2022-06-24 14:46:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zoobuch';
+  title = 'Zoobuch';
+
+  animals : Animal [] = [
+    {
+      name: "Aldo",
+      type: "Hund"  
+    },
+    {
+      name: "Ben",
+      type: "Bär"  
+    },
+    {
+      name: "Schildi",
+      type: "Schildkröte"  
+    },
+    {
+      name: "Lala",
+      type: "Strauss"  
+    },
+  ];
+
+  newAnimal : Animal = {
+    name : "New name",
+    type : "New type"
+  }
+
+  deleteAnimal(animal: Animal) : void {
+    this.animals = this.animals.filter(a => a != animal)
+  }
+
+  addAnimal() : void {
+    this.animals.push(this.newAnimal)
+  }
 }
